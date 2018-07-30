ee1fe5996f617cb76088cfa16cb3ea4e915facfb
Quentin BUATHIER <qbuathier@tetrane.com>
2018-07-30 12:04:09 +0200

    vbox: Add the option machinereadable and details to snapshot showvminfo
    Summary: There is already an option `--machinereadable` and `--details` for the command `vboxmanage showvminfo [vm_name|uuid]`. But the command `vboxmanage snapshot [vm_name|uuid] showvminfo [snapshot_name|uuid]` doesn't have them. So, I did had them to the command for uniformization and because I need it to extract the FS.

--- src/VBox/Frontends/VBoxManage/VBoxManageSnapshot.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageSnapshot.cpp
@@ -621,22 +621,64 @@ RTEXITCODE handleSnapshot(HandlerArg *a)
             setCurrentSubcommand(HELP_SCOPE_SNAPSHOT_SHOWVMINFO);
 
             /* exactly one parameter: snapshot name */
-            if (a->argc != 3)
+            if (a->argc < 3)
             {
                 errorSyntax("Expecting snapshot name only");
                 rc = E_FAIL;
                 break;
             }
+            bool fDetails = false;
+            bool fMachinereadable = false;
+
+            static const RTGETOPTDEF s_aShowVMInfoOptions[] =
+            {
+                { "--details",          'D', RTGETOPT_REQ_NOTHING },
+                { "--machinereadable",  'M', RTGETOPT_REQ_NOTHING },
+            };
+
+            RTGETOPTSTATE GetState;
+            RTGetOptInit(&GetState, a->argc, a->argv, s_aShowVMInfoOptions, RT_ELEMENTS(s_aShowVMInfoOptions),
+                         3, RTGETOPTINIT_FLAGS_NO_STD_OPTS);
+            int ch;
+            RTGETOPTUNION Value;
+            while (   SUCCEEDED(rc)
+                   && (ch = RTGetOpt(&GetState, &Value)))
+            {
+                switch (ch)
+                {
+                    case 'D':   // --details
+                        fDetails = true;
+                        break;
+
+                    case 'M':   // --machinereadable
+                        fMachinereadable = true;
+                        break;
+
+                    default:
+                        errorSyntax("Unknown argument");
+                        rc = E_FAIL;
+                        break;
+                }
+            }
+            if (FAILED(rc))
+                break;
 
             ComPtr<ISnapshot> pSnapshot;
 
             CHECK_ERROR_BREAK(sessionMachine, FindSnapshot(Bstr(a->argv[2]).raw(),
                                                            pSnapshot.asOutParam()));
 
+            VMINFO_DETAILS details = VMINFO_NONE;
+            if (fMachinereadable)
+                details = VMINFO_MACHINEREADABLE;
+            else if (fDetails)
+                details = VMINFO_FULL;
+
+
             /* get the machine of the given snapshot */
             ComPtr<IMachine> pMachine2;
             pSnapshot->COMGETTER(Machine)(pMachine2.asOutParam());
-            showVMInfo(a->virtualBox, pMachine2, NULL, VMINFO_NONE);
+            showVMInfo(a->virtualBox, pMachine2, NULL, details);
         }
         else if (!strcmp(a->argv[1], "list"))
         {
